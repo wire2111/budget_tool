b1c0df3757aa765265b603f9b554769755390cf6
test name <1moment@gmail.com>
2018-02-15 00:17:10 -0700

    pycharm stop bugging me pep8

--- scrubbed.py
+++ scrubbed.py
@@ -17,7 +17,9 @@ use this to do budget after
 # this form of transaction is essentially a dictionary, but you
 # might want to add functionality to it and it allows easy instantiation
 # of transactional records
-class Transaction(Object):
+
+
+class Transaction(object):
     def __init__(self, date, entity, category, description, amount, notes):
         self.date = date
         self.entity = entity
@@ -28,7 +30,8 @@ class Transaction(Object):
 
 # might want to change def __str__ and/or def __repr__ for easy formatting
 
-class AccountReporter(Object):
+
+class AccountReporter(object):
     def __init__(self, argv):
         parser = argparse.ArgumentParser()
         parser.add_argument("-balance_sheets",
@@ -40,32 +43,39 @@ class AccountReporter(Object):
                             help="Save directory for reports")
         args = parser.parse_args(argv)
         self.balance_sheets, self.save = args.balance_sheets, args.save
-        self.transactions = set() # set because we do not want to allow duplicate
-                                  # transactions, and we can easily overwrite them
+        self.transactions = set()
+        # set because we do not want to allow duplicate
+        # transactions, and we can easily overwrite them
         self.net_balance = 0
         self.net_expenditures = 0
         self.net_income = 0
 
     def parse_balance_sheets(self):
+        pass
         # parses balance sheets from self.files,
         # updates self vars, most importantly self.transactions
         # returns 0 if success, some error if failure
 
     def parse_balance_sheet(self, balance_sheet):
-        # parses balance sheet from specified filepath: balance_sheet
-        # make sure to consider how to handle if the filepath doesn't exist
-        # or is the wrong filetype or cannot otherwise be parsed.
+        pass
+        # parses balance sheet from specified file path: balance_sheet
+        # make sure to consider how to handle if the file path doesn't exist
+        # or is the wrong file type or cannot otherwise be parsed.
 
     def print_balance_report(self):
+        pass
         # prints a formatted balance report to console from transactions
 
     def save_balance_report(self):
+        pass
         # saves a copy of the formatted balance report to a file
 
     def print_category_report(self, category):
+        pass
         # this should be self-explanatory at this point
 
-etc
+# etc
+
 
 def app(argv):
     # functionality when run as a script goes here
@@ -84,4 +94,4 @@ def app(argv):
 
 
 if __name__ == '__main__':
-    sys.exit(app(argv[1:]))
+    sys.exit(app(sys.argv[1:]))
