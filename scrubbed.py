6c03ca6d381a54d158601480ff90e40df03af1db
test name <1moment@gmail.com>
2018-02-14 23:48:55 -0700

    Revert "syntax and pep8 stuff"
    This reverts commit b00f9dff10947d329fc4fe6917fe641f3e2e451d.

--- scrubbed.py
+++ scrubbed.py
@@ -2,7 +2,7 @@
 
 import argparse
 import sys
-
+# import os, etc
 
 '''
 import all transactions
@@ -17,9 +17,7 @@ use this to do budget after
 # this form of transaction is essentially a dictionary, but you
 # might want to add functionality to it and it allows easy instantiation
 # of transactional records
-
-
-class Transaction:
+class Transaction(Object):
     def __init__(self, date, entity, category, description, amount, notes):
         self.date = date
         self.entity = entity
@@ -30,8 +28,7 @@ class Transaction:
 
 # might want to change def __str__ and/or def __repr__ for easy formatting
 
-
-class AccountReporter:
+class AccountReporter(Object):
     def __init__(self, argv):
         parser = argparse.ArgumentParser()
         parser.add_argument("-balance_sheets",
@@ -43,38 +40,32 @@ class AccountReporter:
                             help="Save directory for reports")
         args = parser.parse_args(argv)
         self.balance_sheets, self.save = args.balance_sheets, args.save
-        self.transactions = set()   # set because we do not want to allow duplicate
-                                    # transactions, and we can easily overwrite them
+        self.transactions = set() # set because we do not want to allow duplicate
+                                  # transactions, and we can easily overwrite them
         self.net_balance = 0
         self.net_expenditures = 0
         self.net_income = 0
 
     def parse_balance_sheets(self):
-        pass
         # parses balance sheets from self.files,
         # updates self vars, most importantly self.transactions
         # returns 0 if success, some error if failure
 
     def parse_balance_sheet(self, balance_sheet):
-        pass
         # parses balance sheet from specified filepath: balance_sheet
         # make sure to consider how to handle if the filepath doesn't exist
         # or is the wrong filetype or cannot otherwise be parsed.
 
     def print_balance_report(self):
-        pass
         # prints a formatted balance report to console from transactions
 
     def save_balance_report(self):
-        pass
         # saves a copy of the formatted balance report to a file
 
     def print_category_report(self, category):
-        pass
         # this should be self-explanatory at this point
 
-
-# etc
+etc
 
 def app(argv):
     # functionality when run as a script goes here
@@ -93,4 +84,4 @@ def app(argv):
 
 
 if __name__ == '__main__':
-    sys.exit(app(sys.argv[1:]))
+    sys.exit(app(argv[1:]))
