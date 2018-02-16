28acd4b0c0e3272801f611693cbf52c2f961cb81
test name <1moment@gmail.com>
2018-02-16 15:00:36 -0700

    formatting changes

--- scrubbed.py
+++ scrubbed.py
@@ -30,13 +30,17 @@ class Transaction(object):
         self.notes = notes
 
     def __str__(self):
-        return 'Date: {} Entity: {} Amount: {}'.format(self.date, self.entity, self.amount)
+        return 'Date: {} Entity: {} Amount: {}'.format(
+            self.date,
+            self.entity,
+            self.amount)
 
     def __repr__(self):
-        return 'Transaction(Date: {} Entity: {} Amount: {} Origin: {})'.format(self.date,
-                                                                               self.entity,
-                                                                               self.amount,
-                                                                               self.balance_sheet)
+        return 'Transaction(Date: {} Entity: {} Amount: {} Origin: {})'.format(
+            self.date,
+            self.entity,
+            self.amount,
+            self.balance_sheet)
 
 
 class AccountReporter(object):
@@ -44,7 +48,8 @@ class AccountReporter(object):
         parser = argparse.ArgumentParser()
         parser.add_argument("-ingest",
                             default=None,
-                            help="folder of files to ingest as transaction logs relative to script")
+                            help="folder of files to ingest as transaction\
+                            logs relative to script")
         parser.add_argument("-save",
                             default=None,
                             help="save directory for reports")
@@ -65,7 +70,8 @@ class AccountReporter(object):
         for balance_sheet in os.listdir('.'):
             self.parse_balance_sheet(balance_sheet)
         if self.problem_transactions:
-            pprint.pprint(self.problem_transactions)  # use a better way after debugging to display this?
+            pprint.pprint(self.problem_transactions)
+            # use a better way after debugging to display this?
             raise Exception('Problem records please fix')
         pprint.pprint(self.transactions)  # <-- noob working here
         return 0  # why specifically 0?
@@ -83,7 +89,8 @@ class AccountReporter(object):
                 elif length == 4:  # i dont want this magic number either!
                     date, entity, debit, credit = fields
                 else:
-                    self.problem_transactions.append(line)  # this does not catch all problem cases at all :/
+                    self.problem_transactions.append(line)
+                    # this does not catch all problem cases at all :/
                     continue
 
                 def str_to_float(amount):
@@ -93,19 +100,24 @@ class AccountReporter(object):
                         return float(amount.replace(',', ''))
 
                 date = date.rstrip().lstrip()
-                # the format i am providing it in sometimes has white space on this var
-                # should figure out how to make this a date object so i can sort my transaction list by date
+                '''
+                the format i am providing it in sometimes has white space on
+                this var
+                should figure out how to make this a date object so i can sort
+                my transaction list by date
+                '''
                 credit = str_to_float(credit)
                 debit = str_to_float(debit)
 
                 # is this the right way of 'sanitizing' these inputs?
 
-                self.transactions.append(Transaction(date,
-                                                     entity,
-                                                     'to do still',  # todo category sorting
-                                                     balance_sheet,
-                                                     debit - credit,
-                                                     'not sure'))  # notes
+                self.transactions.append(Transaction(
+                    date,
+                    entity,
+                    'to do still',  # todo category sorting
+                    balance_sheet,
+                    debit - credit,
+                    'not sure'))  # notes
                 self.net_expenditures += debit
                 self.net_income += credit
         # parses balance sheet from specified file path: balance_sheet
@@ -115,7 +127,7 @@ class AccountReporter(object):
     def print_balance_report(self):
         pass
         # prints a formatted balance report to console from transactions
-        # i dont think i can do this until i have categories for transaction objects
+        # i dont think i can do this until i have categories for transactions
 
     def save_balance_report(self):
         pass
